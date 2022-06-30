f5e45cb2da433cc451cb04476db990447dad1843
Luis Italo <luisitalomoreirasouza@gmail.com>
2022-06-30 12:10:31 -0300

    implementando metodo put

--- features/step_definitions/get.rb
+++ features/step_definitions/get.rb
@@ -36,3 +36,26 @@ Dado('que o usuario consulte informacoes de funcionario') do
     #utilizar caso api nao retorne string
     #puts @create_employee.parserd_response
   end
+
+  Dado('que o usuario altere uma informacao de funcionario') do
+    @get_employee =  HTTParty.get('http://dummy.restapiexample.com/api/v1/employees', :headers => {'Content-Type': 'application/jason'})
+    puts @get_employee['data'][0]['id']
+    @put_url = 'http://dummy.restapiexample.com/api/v1/update/' + @get_employee['data'][0]['id'].to_s
+  end
+  
+  Quando('ele enviar as novas informacoes') do
+    @update_employee = HTTParty.put(@put_url, :headers => {'Content-Type': 'application/json'}, body: {
+      "employee_name": "credo",
+      "employee_salary": 100,
+      "employee_age": 23,
+      "profile_image": ""
+    }.to_json)
+  end
+  
+  Entao('as informacoes serao alteradas') do
+    expect(@update_employee.code).to eql (200)
+    expect(@update_employee.msg).to eql 'OK'
+    expect(@update_employee["status"]).to eql 'success'
+    expect(@update_employee["message"]).to eql 'Successfully! Record has been updated.'
+    expect(@update_employee["data"]["employee_name"]).to eql 'credo'
+  end
